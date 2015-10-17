085645fdc91f192d71a92d989635ff2b1fb4f073
AprilOrange <aprilorange.net@icloud.com>
2015-10-17 13:28:05 +0800

    fix path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var path = require('path')
 
 function root (dir) {
   dir = dir || ''
-  return path.join(__dirname, '..', dir)
+  return path.join(__dirname, '../..', dir)
 }
 
 module.exports = root
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-var root = require('./lib')
+var root = require('./')
 var should = require('should')
 var path = require('path')
 
 describe('root', () => {
   it('should return curent dir path', () => {
-    root().should.equal(path.join(__dirname))
+    root().should.equal(path.join(__dirname, '../..'))
   })
 })
 
